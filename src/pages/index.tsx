522744bf4d28f66bc46cec648f1e84341f33a239
ryan-zheng <ryanzheng1998@gmail.com>
2022-06-28 22:22:35 +0800

    change ts config, reorganize folder structure and add tailwindcss

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <p className='font-bold'>test</p>
+  )
+}
+
+export default Home
