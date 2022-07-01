56987db470be1a49238b3619d267fbca007cd7b9
ryan-zheng <ryanzheng1998@gmail.com>
2022-07-01 19:41:23 +0800

    Complete: alnalog clock

--- src/pages/analog-clock.tsx
+++ src/pages/analog-clock.tsx
@@ -8,5 +8,63 @@ export default function AnalogClock() {
     setTime(new Date())
   })
 
-  return <p>{time.toString()}</p>
+  const hours = time.getHours()
+  const minutes = time.getMinutes()
+  const seconds = time.getSeconds()
+
+  const hoursDegrees = hours * 30
+  const minutesDegrees = minutes * 6
+  const secondsDegrees = seconds * 6
+
+  return (
+    <div className="grid place-items-center h-screen">
+      <svg
+        className="w-80 aspect-square"
+        viewBox="0 0 100 100"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="50" cy="50" r="50" className="fill-blue-500" />
+        {/* hour hand */}
+        <line
+          x1="50"
+          y1="50"
+          x2="50"
+          y2="25"
+          stroke="black"
+          className="stroke-2"
+          style={{
+            transform: `rotate(${hoursDegrees}deg)`,
+            transformOrigin: '50% 50%',
+          }}
+        />
+        {/* minute hand */}
+        <line
+          x1="50"
+          y1="50"
+          x2="50"
+          y2="10"
+          stroke="black"
+          className="stroke-2"
+          style={{
+            transform: `rotate(${minutesDegrees}deg)`,
+            transformOrigin: '50% 50%',
+          }}
+        />
+        {/* sencond hand */}
+        <line
+          x1="50"
+          y1="50"
+          x2="50"
+          y2="6"
+          stroke="red"
+          className="stroke-1"
+          style={{
+            transform: `rotate(${secondsDegrees}deg)`,
+            transformOrigin: '50% 50%',
+          }}
+        />
+        <circle cx="50" cy="50" r="2" />
+      </svg>
+    </div>
+  )
 }
