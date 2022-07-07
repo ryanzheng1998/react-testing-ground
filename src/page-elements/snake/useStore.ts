8ae62c5523fc0971e91ff024f6eefbcd76ec012b
ryan-zheng <ryanzheng1998@gmail.com>
2022-07-07 23:44:43 +0800

    Rework on tick reducer

--- src/page-elements/snake/useStore.ts
+++ src/page-elements/snake/useStore.ts
@@ -10,8 +10,8 @@ type Direction = 'up' | 'down' | 'left' | 'right'
 export interface State {
   timeStamp: number
   snake: Position[]
-  food: Position | null
-  previousDirection: Direction
+  nextSnakeHead: Position
+  food: Position
   direction: Direction
   speed: number // unit per second
 }
@@ -26,8 +26,8 @@ const initialState: State = {
     { x: 1, y: 3 },
     { x: 0, y: 3 },
   ],
-  food: null,
-  previousDirection: 'right',
+  nextSnakeHead: { x: 6, y: 3 },
+  food: { x: 10, y: 3 },
   direction: 'right',
   speed: 1,
 }
@@ -49,68 +49,10 @@ const reducer = (state: State, action: Action): State => {
       }
     case 'Tick': {
       const timeDelta = action.time - state.timeStamp
-      const distanceToMove = (state.speed * timeDelta) / 1000
-      const snakeHead = state.snake.at(0)
-      if (snakeHead === undefined) throw new Error('snake head is undefined')
-      const remainingDistance = (() => {
-        switch (state.previousDirection) {
-          case 'up':
-            return snakeHead.y % 1
-          case 'down':
-            return 1 - (snakeHead.y % 1)
-          case 'left':
-            return snakeHead.x % 1
-          case 'right':
-            return 1 - (snakeHead.x % 1)
-        }
-      })()
-
-      const directionToCoordinate = {
-        up: { x: 0, y: -1 },
-        down: { x: 0, y: 1 },
-        left: { x: -1, y: 0 },
-        right: { x: 1, y: 0 },
-      }
-      const previousCoordinate = directionToCoordinate[state.previousDirection]
-
-      if (remainingDistance > distanceToMove) {
-        const newSnake = state.snake.map((position) => {
-          return {
-            x: position.x + previousCoordinate.x * distanceToMove,
-            y: position.y + previousCoordinate.y * distanceToMove,
-          }
-        })
-
-        return {
-          ...state,
-          timeStamp: action.time,
-          snake: newSnake,
-        }
-      }
-
-      const distanceToNewDirection = distanceToMove - remainingDistance
-
-      const coordinate = directionToCoordinate[state.direction]
-
-      const newSnake1 = state.snake.map((position) => {
-        return {
-          x: Math.round(position.x + previousCoordinate.x * remainingDistance),
-          y: Math.round(position.y + previousCoordinate.y * remainingDistance),
-        }
-      })
-
-      const newSnake2 = newSnake1.map((position) => {
-        return {
-          x: position.x + coordinate.x * distanceToNewDirection,
-          y: position.y + coordinate.y * distanceToNewDirection,
-        }
-      })
+      const updatePerMs = 1000 / state.speed
 
       return {
         ...state,
-        timeStamp: action.time,
-        snake: newSnake2,
-        previousDirection: state.direction,
       }
     }
   }
