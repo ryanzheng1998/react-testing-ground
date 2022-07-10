bca2d09eb498bd87ca145012a7f5727ae04c2edb
ryan-zheng <ryanzheng1998@gmail.com>
2022-07-10 20:51:13 +0800

    temp

--- src/page-elements/snake/useStore.ts
+++ src/page-elements/snake/useStore.ts
@@ -51,7 +51,6 @@ const reducer = (state: State, action: Action): State => {
       const timeDelta = action.time - state.timeStamp
       const updatePerMs = 1000 / state.speed
 
-      //
       return {
         ...state,
       }
